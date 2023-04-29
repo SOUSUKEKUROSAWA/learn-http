45099c8f0c186ffe0420fa2dda12494ab97f1db6
SOUSUKEKUROSAWA <kurosocavs0714@gmail.com>
2023-04-29 22:46:11 +0900

    Async JavaScriptDD15D15DDDDDD15D15DDDDDDFix CCDDDCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC

--- push-to-github.sh
+++ push-to-github.sh
@@ -8,8 +8,11 @@ if [ "$ANSWER" = "y" ]
 then
     read -p $'\nInput the commit message: ' COMMIT_MESSAGE
 
+    # Remove non-alphanumeric and non-space characters from the commit message
+    CLEANED_COMMIT_MESSAGE=$(echo "$COMMIT_MESSAGE" | sed 's/[^a-zA-Z0-9[:space:]]//g')
+
     git add .
-    git commit -m "$COMMIT_MESSAGE"
+    git commit -m "$CLEANED_COMMIT_MESSAGE"
     git push origin main
 
     echo -e "\nPush to GitHub is completed."
